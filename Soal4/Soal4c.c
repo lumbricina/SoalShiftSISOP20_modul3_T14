870de2f7cf5437fa2ef2cbd83a2153f891c344c3
Falconozura <57929995+Falconozura@users.noreply.github.com>
2020-04-11 18:58:01 +0700

    Update Soal4c.c

--- Soal4/Soal4c.c
+++ Soal4/Soal4c.c
@@ -1,7 +1,30 @@
+#include<stdio.h> 
+#include<stdlib.h> 
+#include<unistd.h> 
+#include<sys/types.h> 
+#include<string.h> 
+#include<sys/wait.h> 
  
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
-#include <unistd.h>
+int pid;
+int pipe1[2];
+ 
+int main()
+{
+    if (pipe(pipe1) == -1)
+    exit(1);
+ 
+    if ((fork()) == 0) {
+        dup2(pipe1[1], 1);
+        close(pipe1[0]);
+        close(pipe1[1]);
+        char *argv1[] = {"ls", NULL};
+        execv("/bin/ls", argv1);
+    }
+    else {
+        dup2(pipe1[0],0);
+        close(pipe1[0]);
+        close(pipe1[1]);
+        char *argv1[] = {"wc", "-l", NULL};
+        execv("/usr/bin/wc", argv1);
+    }
+}
